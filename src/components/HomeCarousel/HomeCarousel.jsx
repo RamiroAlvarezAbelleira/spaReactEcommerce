fa2f1fae111e1067a95fa6b8c685f2a9989743d4
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-10-28 12:56:35 -0300

    added slides to the home page

--- src/components/HomeCarousel/HomeCarousel.jsx
+++ src/components/HomeCarousel/HomeCarousel.jsx
@@ -1,6 +1,7 @@
 import Carousel from 'react-bootstrap/Carousel';
-import slider1 from '../../assets/images/slider-1.jpg'
-import slider2 from '../../assets/images/slider-2.jpg'
+import slider1 from '../../assets/images/slide-test1.jpg'
+import slider2 from '../../assets/images/slide-test2.jpg'
+import slider3 from '../../assets/images/slide-test.jpg'
 
 function HomeCarousel() {
   return (
@@ -16,6 +17,13 @@ function HomeCarousel() {
         <img
           className="d-block w-100"
           src={slider2}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className='mx-0 px-0'>
+        <img
+          className="d-block w-100"
+          src={slider3}
           alt="Second slide"
         />
       </Carousel.Item>
