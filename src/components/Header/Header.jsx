16d8748e4ca29d442616bb7f9b56eada925c5522
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-11-03 15:58:00 -0300

    started working on the search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,42 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap'
+import { NavLink, useNavigate } from 'react-router-dom';
 import image from "../../assets/images/logo-BM.png";
 
 function Header() {
-  let active = 'col-md-3 col-xl-2 text-center text-decoration-none text-white'
-  let notActive = 'col-md-3 col-xl-2 text-center text-decoration-none text-gray-500'
+  /* search query logic */
+
+  const navigate = useNavigate()
+
+  const handleSearch = () => {
+
+  }
+
+  const [searchedProducts, setSearchedProducts] = useState([])
+  const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    if (!keyword === '') {
+      let url = `http://localhost:3000/productos?search=${keyword}`
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          if (Array.isArray(data.data)) {
+            setSearchedProducts(data.data)
+          } else {
+            setSearchedProducts([])
+          }
+        })
+    }
+  }, [keyword])
+
+
+  /* active and inactive link styles*/
+
+  let active = 'col-md-4 col-xl-2 text-center text-decoration-none text-white'
+  let notActive = 'col-md-4 col-xl-2 text-center text-decoration-none text-gray-500'
+
   return (
     <Navbar bg="dark" variant='dark' expand="md">
       <Navbar.Brand href="/"><NavLink to="/"><img src={image} alt="BiciMundo" width="100" /></NavLink></Navbar.Brand>
@@ -12,19 +44,28 @@ function Header() {
       <Navbar.Collapse id="basic-navbar-nav  d-flex">
         <Nav className='container-fluid mx-0 justify-content-between'>
           <Container className="row justify-content-start">
-            <NavLink to="/" end className={({isActive}) => ( isActive ? active : notActive )}  >
+            <NavLink to="/" end className={({ isActive }) => (isActive ? active : notActive)}  >
               Home
             </NavLink>
-            <NavLink to="/productos" className={({isActive}) => ( isActive ? active : notActive )} >
+            <NavLink to="/productos" className={({ isActive }) => (isActive ? active : notActive)} >
               Productos
             </NavLink>
           </Container>
+          <Form className="d-flex w-100">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Buscar</Button>
+          </Form>
           <Container className="row justify-content-end">
-            <NavLink to="/login" className={({isActive}) => ( isActive ? active : notActive )} >
-              Login
+            <NavLink to="/register" className={({ isActive }) => (isActive ? active : notActive)} >
+              Registro
             </NavLink>
-            <NavLink to="/register" className={({isActive}) => ( isActive ? active : notActive )} >
-              Register
+            <NavLink to="/login" className={({ isActive }) => (isActive ? active : notActive)} >
+              Ingresa
             </NavLink>
           </Container>
         </Nav>
