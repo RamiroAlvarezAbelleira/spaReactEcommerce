04d88d41de96f3e20e44e30a46de67c9225b23ab
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-11-03 22:55:19 -0300

    reset of searchBar branch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,35 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom';
+import { useRef } from 'react';
+import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap'
+import { NavLink, useNavigate } from 'react-router-dom';
 import image from "../../assets/images/logo-BM.png";
 
 function Header() {
-  let active = 'col-md-3 col-xl-2 text-center text-decoration-none text-white'
-  let notActive = 'col-md-3 col-xl-2 text-center text-decoration-none text-gray-500'
+  /* states */
+
+  let keyword;
+
+  /* search query logic */
+
+  const search = useRef()
+
+  const navigate = useNavigate()
+
+  const handleSearch = async (e) => {
+    e.preventDefault()
+    let value = search.current.value
+    if (value) {
+      keyword = value
+    } else {
+      keyword = undefined
+    }
+    navigate('/productos', { state: {keyword: keyword} })
+  }
+
+  /* active and inactive link styles*/
+
+  let active = 'col-md-4 col-xl-3 text-center text-decoration-none text-white'
+  let notActive = 'col-md-4 col-xl-3 text-center text-decoration-none text-gray-500'
+
   return (
     <Navbar bg="dark" variant='dark' expand="md">
       <Navbar.Brand href="/"><NavLink to="/"><img src={image} alt="BiciMundo" width="100" /></NavLink></Navbar.Brand>
@@ -12,19 +37,29 @@ function Header() {
       <Navbar.Collapse id="basic-navbar-nav  d-flex">
         <Nav className='container-fluid mx-0 justify-content-between'>
           <Container className="row justify-content-start">
-            <NavLink to="/" end className={({isActive}) => ( isActive ? active : notActive )}  >
+            <NavLink to="/" end className={({ isActive }) => (isActive ? active : notActive)}  >
               Home
             </NavLink>
-            <NavLink to="/productos" className={({isActive}) => ( isActive ? active : notActive )} >
+            <NavLink to="/productos" className={({ isActive }) => (isActive ? active : notActive)} >
               Productos
             </NavLink>
           </Container>
+          <Form onSubmit={handleSearch} className="d-flex w-100">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+              ref={search}
+            />
+            <Button type='submit' variant="outline-light">Buscar</Button>
+          </Form>
           <Container className="row justify-content-end">
-            <NavLink to="/login" className={({isActive}) => ( isActive ? active : notActive )} >
-              Login
+            <NavLink to="/register" className={({ isActive }) => (isActive ? active : notActive)} >
+              Registro
             </NavLink>
-            <NavLink to="/register" className={({isActive}) => ( isActive ? active : notActive )} >
-              Register
+            <NavLink to="/login" className={({ isActive }) => (isActive ? active : notActive)} >
+              Ingresar
             </NavLink>
           </Container>
         </Nav>
--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -2,20 +2,33 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { Container, Row } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom'
 import { ProductCard } from '../../components/ProductCard'
 
-const Home = () => {
+const Productos = () => {
+    const { state } = useLocation()
+
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        let url = 'http://localhost:3000/productos'
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                setProducts(data.data)
-            })
+        const handleState = async () => {
+            let url;
+            if (state?.keyword !== undefined) {
+                url = `http://localhost:3000/productos?search=${state.keyword}`
+            } else {
+                url = 'http://localhost:3000/productos'
+            }
+            await fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        setProducts(data.data)
+                    })
+        }
 
-    }, [])
+        handleState()
+
+    }, [state])
 
     return (
         <div className='mx-0 px-0 mt-5'>
@@ -33,4 +46,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Productos
