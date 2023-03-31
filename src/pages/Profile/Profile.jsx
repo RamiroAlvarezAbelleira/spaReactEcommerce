3f38bf788a941d6ed4428566ef1bb1315159abe5
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-03-31 15:44:10 -0300

    added basic style to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,7 @@
-import { Table } from "react-bootstrap"
+import { Col, Row, Table } from "react-bootstrap"
 import { useSelector } from "react-redux"
+
+import './Profile.css'
 
 
 const Profile = () => {
@@ -8,28 +10,41 @@ const Profile = () => {
     
   return ( user &&
     <div>
-        <h1>Hello {user.firstName}!</h1>
-        <img src={`http://localhost:3000/images/users/${user.image}`} />
-        <Table striped>
-            <tbody>
-                <tr>
-                    <td>Name</td>
-                    <td>{user.firstName}</td>
-                </tr>
-                <tr>
-                    <td>Lastname</td>
-                    <td>{user.lastName}</td>
-                </tr>
-                <tr>
-                    <td>email</td>
-                    <td>{user.email}</td>
-                </tr>
-                <tr>
-                    <td>birthdate</td>
-                    <td>{user.birthdate}</td>
-                </tr>
-            </tbody>
-        </Table>
+        <Row className="profile-banner">
+            <h1 className="text-dark">Hello {user.firstName}!</h1>
+        </Row>
+        <Row className="profile-details">
+            <Row>
+                <Col xl={2}>
+                    <img className="profile-image" src={`http://localhost:3000/images/users/${user.image}`} />
+                </Col>
+                <Col xl={10} className="d-flex align-items-center">
+                    <h3 className="m-0 text-dark">User details</h3>
+                </Col>
+            </Row>
+            <Row className="profile-table">
+                <Table striped>
+                    <tbody>
+                        <tr>
+                            <td>Name</td>
+                            <td>{user.firstName}</td>
+                        </tr>
+                        <tr>
+                            <td>Lastname</td>
+                            <td>{user.lastName}</td>
+                        </tr>
+                        <tr>
+                            <td>email</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        <tr>
+                            <td className="last-table-row">birthdate</td>
+                            <td className="last-table-row">{user.birthdate}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Row>
+        </Row>
     </div>
   )
 }
