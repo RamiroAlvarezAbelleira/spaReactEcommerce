bae015cebde428c26491d78fb6cd9cd53a2688ca
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-04-25 20:38:28 -0300

    restyled product detail

--- src/pages/Detalle/Detalle.jsx
+++ src/pages/Detalle/Detalle.jsx
@@ -5,6 +5,9 @@ import { BeatLoader } from 'react-spinners'
 import axios from '../../api/axios';
 import './Detalle.css'
 import { ProductSwiper } from '../../components/ProductSwiper';
+import credito from '../../assets/images/tarjetas-de-credito.png'
+import debito from '../../assets/images/tarjetas-de-debito.png'
+import efectivo from '../../assets/images/efectivo.jpeg'
 
 function Detalle() {
     const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -72,7 +75,7 @@ function Detalle() {
                             </div>
                             <div className='specs-container'>
                                 <h4 className='py-5 pl-0 related-products-title'>Especificaciones</h4>
-                                <div className='mt-5 p-3 rounded-3 bg-gray-200'>
+                                <div className='my-5 p-3 w-50 rounded-3 bg-gray-200'>
                                     {product.category && <p className='specs-item'>Categoria: {product.category}</p>}
                                     {product.brand && <p className='specs-item'>Marca: {product.brand}</p>}
                                     {product.model && <p className='specs-item'>Modelo: {product.model}</p>}
@@ -87,10 +90,10 @@ function Detalle() {
                             </div>
                             <div className='specs-container'>
                                 <h4 className='py-5 pl-0 related-products-title'>Descripcion</h4>
-                                <p className='description-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit sit libero non similique, vel saepe nostrum nam sint, veniam id doloribus omnis. Nihil nemo amet porro id vitae magnam quia, architecto quisquam pariatur cupiditate, accusantium beatae animi, dolor laborum! Error veritatis mollitia repudiandae nulla aperiam soluta corrupti aliquam iusto minus. Culpa nostrum voluptatum, illum, quo excepturi voluptas facilis est fuga provident soluta veniam atque sequi nisi delectus sed. Fugiat sequi at commodi incidunt sunt eveniet sit doloribus blanditiis nihil soluta adipisci dolore, error et ipsum ab omnis cumque, dolorem praesentium dolores laborum vero officia vel enim sed? Minus voluptatibus repudiandae id atque maiores ad ratione, cupiditate ullam explicabo voluptate incidunt cumque cum animi, saepe totam reiciendis accusamus corporis vel magni. Quibusdam aut tempora accusantium obcaecati veritatis labore maiores consectetur rem possimus amet iusto quas recusandae animi ut perspiciatis sit dignissimos officiis vel atque totam porro debitis eius, molestias itaque. Error, nihil vel dolor ex fugit hic placeat aperiam vero maiores. Beatae, deserunt rem, nam aperiam sapiente perferendis dolorem perspiciatis sunt fuga, eaque hic adipisci voluptate quisquam? Accusamus ipsum, quisquam perferendis saepe neque excepturi dicta eligendi nulla quod ab iure id voluptatem dolore explicabo assumenda, non perspiciatis. Provident quidem nemo nesciunt!</p>
+                                <p className='description-text mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit sit libero non similique, vel saepe nostrum nam sint, veniam id doloribus omnis. Nihil nemo amet porro id vitae magnam quia, architecto quisquam pariatur cupiditate, accusantium beatae animi, dolor laborum! Error veritatis mollitia repudiandae nulla aperiam soluta corrupti aliquam iusto minus. Culpa nostrum voluptatum, illum, quo excepturi voluptas facilis est fuga provident soluta veniam atque sequi nisi delectus sed. Fugiat sequi at commodi incidunt sunt eveniet sit doloribus blanditiis nihil soluta adipisci dolore, error et ipsum ab omnis cumque, dolorem praesentium dolores laborum vero officia vel enim sed? Minus voluptatibus repudiandae id atque maiores ad ratione, cupiditate ullam explicabo voluptate incidunt cumque cum animi, saepe totam reiciendis accusamus corporis vel magni. Quibusdam aut tempora accusantium obcaecati veritatis labore maiores consectetur rem possimus amet iusto quas recusandae animi ut perspiciatis sit dignissimos officiis vel atque totam porro debitis eius, molestias itaque. Error, nihil vel dolor ex fugit hic placeat aperiam vero maiores. Beatae, deserunt rem, nam aperiam sapiente perferendis dolorem perspiciatis sunt fuga, eaque hic adipisci voluptate quisquam? Accusamus ipsum, quisquam perferendis saepe neque excepturi dicta eligendi nulla quod ab iure id voluptatem dolore explicabo assumenda, non perspiciatis. Provident quidem nemo nesciunt!</p>
                             </div>
                         </Col>
-                        <Col sm={{ span: 3 }} className='p-0'>
+                        <Col sm={{ span: 3 }} className='p-0 payment-col'>
                             <div className='price-title-container'>
                                 <div>
                                     <h1 className='detail-description pb-4'>{product.description}</h1>
@@ -100,14 +103,31 @@ function Detalle() {
                                         {product.discount > 0 && <span className='detail-discount'> {product.discount}% OFF</span>}
                                     </h2>
                                 </div>
+                                <div className='mt-4'>
+                                    <p className='detail-discount'>Envio gratis!</p>
+                                    <p><span className='detail-price'>Stock: </span>22 disponibles</p>
+                                </div>
                                 <Button variant='dark' className='detail-add-to-cart my-5'>Agregar al carrito</Button>
-                                <div>
-                                    <p>22 disponibles</p>
-                                    <p>Envio gratis!</p>
+                                <div className='mt-4'>
+                                    <p><span className='detail-discount'>Devolución gratis.</span> Tenés 30 días desde que lo recibís.</p>
+                                    <p><span className='detail-discount'>Compra Protegida</span>, recibí el producto que esperabas o te devolvemos tu dinero.</p>
+                                    <p>12 meses de garantía de fábrica.</p>
                                 </div>
                             </div>
                             <div className='price-title-container mt-4'>
-                                <h2>metodos de pago</h2>
+                                <h2 className='detail-description pb-4'>Medios de pago</h2>
+                                <div className='mt-5'>
+                                    <h5 className='pb-3'>Tarjetas de credito.</h5>
+                                    <img className='w-100' src={credito} alt='Tarjetas de credito' />
+                                </div>
+                                <div className='mt-5'>
+                                    <h5 className='pb-3'>Tarjetas de debito.</h5>
+                                    <img className='w-100' src={debito} alt='Tarjetas de debito' />
+                                </div>
+                                <div className='mt-5'>
+                                    <h5 className='pb-3'>Efectivo.</h5>
+                                    <img className='w-50' src={efectivo} alt='Efectivo' />
+                                </div>
                             </div>
                         </Col>
                         <Col md={{ span: 6, offset: 1 }} >
