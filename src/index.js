f7a1622353a37501f4bac7d99df1864a7afcc588
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-06-15 17:07:03 -0300

    railway test

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React>
 );
 
 reportWebVitals();
