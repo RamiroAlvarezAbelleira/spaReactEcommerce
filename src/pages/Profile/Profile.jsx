c00319aad2cae685e518ee1707532ba6933a5702
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-08-14 13:38:47 -0300

    profile now responsive

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -12,14 +12,14 @@ const Profile = () => {
   return ( user &&
     <div>
         <Row className="profile-banner d-flex justify-content-center">
-            <h1 className='bg-dark text-light text-center w-50 rounded-pill'>Hello {user.firstName}!</h1>
+            <h1 className='bg-dark text-light text-center w-50 rounded-pill profile-title'>Hello {user.firstName}!</h1>
         </Row>
         <Row className="profile-details">
-            <Row>
-                <Col xl={2}>
+            <Row className="image-and-name-row">
+                <Col className="d-flex align-items-center">
                     <img className="profile-image" src={`https://apiecommerce-development.up.railway.app/images/users/${user.image}`} alt='user'/>
                 </Col>
-                <Col xl={10} className="d-flex align-items-center">
+                <Col className="d-flex align-items-center">
                     <h3 className="m-0 text-dark">User details</h3>
                 </Col>
             </Row>
@@ -46,7 +46,7 @@ const Profile = () => {
                 </Table>
             </Row>
             <Row className="d-flex justify-content-end align-self-end">
-                <Link className='w-25 align-self-end mb-5 mr-2' to={`/perfil/edit`}><Button className="w-100" variant="secondary">Editar</Button></Link>
+                <Link className='w-25 align-self-end mb-5 mr-2 mobile-edit-link' to={`/perfil/edit`}><Button className="w-100" variant="secondary">Editar</Button></Link>
             </Row>
             
         </Row>
